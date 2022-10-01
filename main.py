b050319fb4ce823e727dda022bd1f046f537b662
student vova <popihacvova@gmail.com>
2022-10-01 10:38:15 +0300

    Update: feature/lab_ex_5

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ if num1 > num2:
     num1 = temp
     num2 = num1
     temp = num2
+number = num1
+
+if number % 2 == 0:
+    number += 1
+
+while number < num2:
+    print(f'{number}')
+    number += 2
+
+print('Робота програми завершена')
