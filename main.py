1b9991b60d5175a59afbc7ca53d5cc8372bd71b0
student vova <popihacvova@gmail.com>
2022-10-01 11:11:47 +0300

    Update: feature/lab_ex_5

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 num1 = int(input('Enter first number->'))
 num2 = int(input('Enter second number->'))
-number = num1
 
+if num1 > num2:
+    temp = num1
+    num1 = num2
+    num2 = temp
 
-while number < num2:
-    print(f'number = {number}')
-    number -= 1
-else:
-    print(f'number = {number}. Робота циклу завершена')
+while num1>num2:
+    print(f'num1 = {number}')
+    num1-=1
+ print(f'number = {number}. Робота циклу завершена')
