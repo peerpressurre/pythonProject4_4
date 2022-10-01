9f37fac288e109a303c82ba0e88d086aa18d4a5b
student vova <popihacvova@gmail.com>
2022-10-01 11:06:55 +0300

    Update: feature/lab_ex_5

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 num1 = int(input('Enter first number->'))
 num2 = int(input('Enter second number->'))
-number = num2
+number = num1
 
-while num1 < number < num2:
+
+while number < num2:
     print(f'number = {number}')
     number -= 1
 else:
