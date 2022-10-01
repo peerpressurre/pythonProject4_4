1bd646fba4a3c33419cd4b14b4522a05b7741e5f
student vova <popihacvova@gmail.com>
2022-10-01 11:27:22 +0300

    Update: feature/lab_ex_5

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 num1 = int(input('Enter first number->'))
 num2 = int(input('Enter second number->'))
-
 if num1 > num2:
     temp = num1
     num1 = num2
@@ -8,7 +7,7 @@ if num1 > num2:
 number = num2
 
 while number > num1:
-    print(f'{number}5')
+    print(f'{number}')
     number -= 1
 else:
     print(f'{number}')
