19d897502f5c74893b2c6f461fa2b1da94106619
student vova <popihacvova@gmail.com>
2022-10-01 10:08:51 +0300

    Update: feature/lab_ex_2

---
+++ main.py
@@ -0,0 +1,8 @@
+num1 = int(input('Enter first number->'))
+num2 = int(input('Enter second number->'))
+number = num1
+if number % 2 == 0:
+    number += 1
+else:
+ while number < num2:
+     print(f'')
