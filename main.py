238338d28140826f5c773cac9bf735a22c3d20f0
student vova <popihacvova@gmail.com>
2022-10-01 10:13:24 +0300

    Update: feature/lab_ex_2

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ num2 = int(input('Enter second number->'))
 number = num1
 if number % 2 == 0:
     number += 1
-else:
- while number < num2:
-     print(f'{number}')
-     number += 2
- else:
-     print(f'{number}. Робота циклу завершена')
+
+while number < num2:
+    print(f'{number}')
+    number += 2
+
 print('Робота програми завершена')
