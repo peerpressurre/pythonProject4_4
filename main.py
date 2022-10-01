fbdcbb7fa5112f3169471ee6deba7aa008334cbd
student vova <popihacvova@gmail.com>
2022-10-01 10:29:35 +0300

    Update: feature/lab_ex_4

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ num1 = int(input('Enter first number->'))
 num2 = int(input('Enter second number->'))
 number = num2
 
-while number < num2:
+while num1 < number < num2:
     print(f'number = {number}')
-    number += 1
+    number -= 1
 else:
     print(f'number = {number}. Робота циклу завершена')
-print('Робота программи завершена')
