2c5c37644f9f8a15bd7dd34cdcb08e837f8e2b6a
student vova <popihacvova@gmail.com>
2022-10-01 10:12:16 +0300

    Update: feature/lab_ex_2

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ if number % 2 == 0:
     number += 1
 else:
  while number < num2:
-     print(f'')
+     print(f'{number}')
+     number += 2
+ else:
+     print(f'{number}. Робота циклу завершена')
+print('Робота програми завершена')
